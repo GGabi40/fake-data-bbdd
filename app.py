1b15fadb55f2d09c65b0e88bf40486a062fa0027
ggabi40 <gabibc2000@hotmail.com>
2024-11-07 20:04:08 -0300

    'Fake user'

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request
+from faker import Faker
+import random
+
+app = Flask(__name__)
+faker = Faker('es_ES')
+
+fake_professions = [
+    "Ingeniero en realidad aumentada",
+    "Director de tecnología de drones",
+    "Consultor de redes neuronales cuánticas",
+    "Especialista en análisis de datos de unicornios",
+    "Gerente de automatización de IA",
+    "Diseñador de interfaces holográficas",
+    "Coordinador de experiencias de usuario en metaverso",
+    "Consultor de sostenibilidad espacial",
+    "Desarrollador de algoritmos para robots autómatas",
+    "Analista de tendencias en nanotecnología",
+    "Médico",
+    "Carpintero",
+    "Analista de datos",
+    "Bioquímico",
+    "Farmacéutico",
+    "Odontólogo",
+    "Enfermero",
+    "Piloto de Avión",
+    "Soldado",
+    "Vendedor"
+]
+
+def generate_fake_profession():
+    return random.choice(fake_professions)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    datos_seleccionados = []
+    
+    # Número de usuarios a generar, por defecto 5
+    num_usuarios = 5
+
+    if request.method == 'POST':
+        num_usuarios = int(request.form.get('cantidad', 5))
+        datos_seleccionados = request.form.getlist('datos')
+
+    # Genera la cantidad especificada de usuarios falsos
+    usuarios_falsos = []
+    
+    for i in range(num_usuarios):
+        usuario = {}
+        if 'nombre' in datos_seleccionados:
+            usuario['nombre'] = faker.first_name()
+        if 'apellido' in datos_seleccionados:
+            usuario['apellido'] = faker.last_name()
+        if 'fecha_nacimiento' in datos_seleccionados:
+            usuario['fecha_nacimiento'] = faker.date_of_birth()
+        if 'direccion' in datos_seleccionados:
+            usuario['direccion'] = faker.address()
+        if 'email' in datos_seleccionados:
+            usuario['email'] = faker.email()
+        if 'telefono' in datos_seleccionados:
+            usuario['telefono'] = faker.phone_number()
+        if 'texto' in datos_seleccionados:
+            usuario['texto'] = faker.text()
+        if "profesiones" in datos_seleccionados:
+            usuario["profesion"] = generate_fake_profession()
+        
+        usuarios_falsos.append(usuario)
+        
+    
+    return render_template('index.html', usuarios=usuarios_falsos, cantidad=num_usuarios, datos_seleccionados=datos_seleccionados)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,29 @@
+document.getElementById("generate-sql-btn").addEventListener("click", function () {
+    const table = document.querySelector("table");
+
+    let selectedData = [];
+    // Recorremos los checkboxes seleccionados
+    document.querySelectorAll("input[name='datos']:checked").forEach(
+        function (checkbox) {
+        selectedData.push(checkbox.value); // Guardamos el valor de los checkboxes
+      }
+    );
+
+    let sqlOutput = "INSERT INTO nombre_tabla (" + selectedData.join(", ") + ") VALUES\n";
+
+    const rows = table.querySelectorAll("tbody tr");
+    rows.forEach((row, index) => {
+      const cells = row.querySelectorAll("td");
+      const values = Array.from(cells).map((cell) => `'${cell.innerText}'`);
+      sqlOutput += `(${values.join(", ")})`;
+
+      // Agrega punto y coma al último dato
+      if (index < rows.length - 1) {
+        sqlOutput += ",\n";
+      } else {
+        sqlOutput += ";";
+      }
+    });
+
+    document.getElementById("sql-output").value = sqlOutput;
+  });
