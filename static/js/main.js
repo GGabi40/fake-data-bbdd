a76fdcc2a72e0059b3ab03b16d2e5ec692aa3c31
ggabi40 <gabibc2000@hotmail.com>
2024-11-07 23:32:48 -0300

    update JS

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,11 @@ document.getElementById("generate-sql-btn").addEventListener("click", function (
     const table = document.querySelector("table");
 
     let selectedData = [];
+
     // Recorremos los checkboxes seleccionados
-    document.querySelectorAll("input[name='datos']:checked").forEach(
-        function (checkbox) {
-        selectedData.push(checkbox.value); // Guardamos el valor de los checkboxes
+    document.querySelectorAll("input[name='datos']:checked")
+    .forEach( checkbox => {
+        selectedData.push(checkbox.value); // Se guarda el valor de los checkboxes
       }
     );
 
@@ -14,7 +15,12 @@ document.getElementById("generate-sql-btn").addEventListener("click", function (
     const rows = table.querySelectorAll("tbody tr");
     rows.forEach((row, index) => {
       const cells = row.querySelectorAll("td");
-      const values = Array.from(cells).map((cell) => `'${cell.innerText}'`);
+      const values = Array.from(cells).map((cell) => {
+        const cellValue = cell.innerText.trim();
+        // si no es un numero, tendrá comillas simple, sino, va sin comilla
+        return isNaN(cellValue) ? `'${cellValue}'` : cellValue;
+      });
+
       sqlOutput += `(${values.join(", ")})`;
 
       // Agrega punto y coma al último dato
@@ -29,6 +35,7 @@ document.getElementById("generate-sql-btn").addEventListener("click", function (
 });
 
 
+// COPIAR TEXTO de TEXTAREA
 let texto = document.getElementById("sql-output").value;
 const copiar = async () => {
     try {
